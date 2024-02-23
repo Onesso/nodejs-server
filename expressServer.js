ff254c7f5d55ba571e1416cc4e0e45201d01e97c
Frank Onesso <odhiambofrank1965@gmail.com>
2024-02-23 13:42:10 +0300

    made an express server with res this is home page on port 3000

---
+++ expressServer.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.all("*", (req, res) => {
+  res.send("<h1> this is the home page </h1>");
+});
+
+app.listen(3000);
+
+console.log("app is listeing on port 3000");
